23a822b1bfd26eb98e1d5210ee4dc309bd9ad786
ronanmarkdsouza <ronanmarkdsouza@gmail.com>
2023-03-14 16:27:43 +0530

    added cli for input and output

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <omp.h>
+#include <string.h>
 #include "../include/rocket_param.hpp"
 #include "../include/density_temperature.hpp"
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
 	rocket_params rocket;
-	type_density_temperature test;
-	test = density_temperature(100,100);
-	cout<<test.density<<" "<<test.temperature<<endl;
-	cout<<rocket.aero_data_path<<endl;
+	if(argc==1){
+		cout<<"Usage:\n\t./build/main --[options] [values]\n\tExample: ./build/main --input input.txt --output output.pdf"<<endl;
+		exit(-1);
+	}
+	for(int i=1; i<argc; ++i){
+		if(strcmp(argv[i],"--help")==0)
+			cout<<"Usage:\n\t./build/main --[options] [values]\n\tExample: ./build/main --input input.txt --output output.pdf"<<endl;
+		if(strcmp(argv[i],"--input")==0)
+			cout<<"The input file has been set to "<<argv[i+1]<<endl;
+		if(strcmp(argv[i], "--output")==0)
+			rocket.output_name = string(argv[i+1]);
+	}
+	cout<<rocket.output_name<<endl;
 	return 0;
 }
